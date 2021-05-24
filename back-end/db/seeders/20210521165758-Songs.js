fbda7c250562c85cd7e53e9f5d9ecaaaed8958dd
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-05-24 08:52:10 -0700

    extra seeds

--- back-end/db/seeders/20210521165758-Songs.js
+++ back-end/db/seeders/20210521165758-Songs.js
@@ -11,6 +11,42 @@ module.exports = {
       category: 1,
       createdAt: new Date(),
       updatedAt: new Date()
+    },{
+      name: 'skinz',
+      link: 'https://res.cloudinary.com/hitsman/video/upload/v1621871216/8485_-_skinz_prod._powerdoll_ufatss.mp3',
+      artist: '8485',
+      createdBy: 2,
+      views: 450,
+      category: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: 'I want this song 2 never end',
+      link: 'https://res.cloudinary.com/hitsman/video/upload/v1621871220/8485_i_want_the_song_2_never_end_Prod_by_phixel_bh9z1v.mp3',
+      artist: '8485',
+      createdBy: 3,
+      views: 350,
+      category: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: '115',
+      link: 'https://res.cloudinary.com/hitsman/video/upload/v1621871229/8485_-_115fromtheheart_wywgnb.mp3',
+      artist: '8485',
+      createdBy: 1,
+      views: 250,
+      category: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: 'Hangar',
+      link: 'https://res.cloudinary.com/hitsman/video/upload/v1621871240/8485_-_hangar_figc9e.mp3',
+      artist: '8485',
+      createdBy: 2,
+      views: 150,
+      category: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
     }], {});
   },
 
--- front-end/src/components/SongPage/index.js
+++ front-end/src/components/SongPage/index.js
@@ -3,20 +3,20 @@ import { useParams } from 'react-router-dom'
 export default function SongPage ({ all }) {
     const { songId } = useParams()
 
-    song = all[songId]
+    const song = all[songId]
 
     return (
         <>
             <div>
-                {song.name}
+                {song?.name}
             </div>
             <div>
-                {song.artist}
+                {song?.artist}
             </div>
-            {/* <div>
-                {song.User.username}
-            </div> */}
-            <audio controls src={song.link} />
+            <div>
+                {song?.User.username}
+            </div>
+            <audio controls src={song?.link} />
         </>
     )
 }
