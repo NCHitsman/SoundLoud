90119c2c30d048729302ca4dea270b3977892160
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-05-27 13:04:32 -0700

    last css touches

--- back-end/db/seeders/20210521173456-Comments.js
+++ back-end/db/seeders/20210521173456-Comments.js
@@ -4,11 +4,126 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Comments', [{
       text: 'Nice song',
+      userId: 3,
+      songId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Your voice tunes the life of lifeless people.',
+      userId: 5,
+      songId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'You are the only person, who I want to listen till my last breathe.',
+      userId: 3,
+      songId: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Such emotion and connection to the song.',
+      userId: 4,
+      songId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Great passion and talent throughout. You convey emotion well.',
+      userId: 2,
+      songId: 9,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'You conveyed the emotion within the lyrics well.',
+      userId: 6,
+      songId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Perform as much as you can as you have bags of potential.',
+      userId: 1,
+      songId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Great musicianship, you play with confidence and vocally you have a really good tone.',
+      userId: 6,
+      songId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Vocally you have a great tone and really good control on the high notes, a great overall performance.',
+      userId: 3,
+      songId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Try to vary it more and give us more dynamics to the performance.',
+      userId: 4,
+      songId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'You have lots of character to your vocal which really assists in delivering the story of the song.',
+      userId: 5,
+      songId: 6,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Your delivery is fantastic which really separates you and makes you stand out from your peers.',
+      userId: 6,
+      songId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Great power and control are all there!',
+      userId: 1,
+      songId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Lots of potential with superb power.',
+      userId: 4,
+      songId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'I keep listening to it over and over again.',
+      userId: 3,
+      songId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'One of the most beautiful and amazing tracks Ive heard.',
+      userId: 4,
+      songId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'thanks this will help me ease into sleep and forget the fact that I missed an exam...',
+      userId: 5,
+      songId: 9,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'Only problem with this mix is its too good, I keep listening to the music rather than working, and looking up pieces so I can have them! :)',
+      userId: 6,
+      songId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      text: 'I think this is the most beautiful mix Ive heard',
       userId: 1,
       songId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
+    },{
+      text: 'perfect.',
+      userId: 2,
+      songId: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
--- front-end/src/components/SongPage/index.js
+++ front-end/src/components/SongPage/index.js
@@ -50,91 +50,89 @@ export default function SongPage({ all }) {
     };
 
     return (
-        <div className='songPage'>
-            <div className='songPage__cont'>
+        <div className='songPage__cont'>
 
-                <div className='song__info__cont'>
+            <div className='song__info__cont'>
 
-                    <div className='song__title__artist'>
+                <div className='song__title__artist'>
 
-                        <img alt='song cover' id='songPage__img' className='s__logo' src={song?.cover}></img>
+                    <img alt='song cover' id='songPage__img' className='s__logo' src={song?.cover}></img>
 
-                        <div className='songPage__info__child__cont'>
-                            <div className='songPage__title__artist__cont'>
-                                <div className='textAlign songPage__info__title'>
-                                    {song?.name}
-                                </div>
-                                <Link className='songPage__info__artist' to={`/users/${song?.User.id}`}>
-                                    {song?.User.username}
-                                </Link>
+                    <div className='songPage__info__child__cont'>
+                        <div className='songPage__title__artist__cont'>
+                            <div className='textAlign songPage__info__title'>
+                                {song?.name}
                             </div>
-                            <div className='songPage__info__artist__cont'>
-                                <div className='songPage__info__views'>
-                                    Views: {song?.views}
-                                </div>
-                                <div className='songPage__info__artist'>
-                                    {song?.Category.name}
-                                </div>
+                            <Link className='songPage__info__artist' to={`/users/${song?.User.id}`}>
+                                {song?.User.username}
+                            </Link>
+                        </div>
+                        <div className='songPage__info__artist__cont'>
+                            <div className='songPage__info__views'>
+                                Views: {song?.views}
+                            </div>
+                            <div className='songPage__info__artist'>
+                                {song?.Category.name}
                             </div>
                         </div>
-
-                    </div>
-
-
-                    <div className='audioHolder'>
-                        <audio className='songPage__audio' controls src={song?.link} />
-                    </div>
-
-                </div>
-
-                <div className={'song__comment__cont ' + commentClass}>
-
-                    <div className='comment__holder'>
-
-                        <div className='li__comment__holder'>
-                            {comments && Object.values(comments).map(comment => {
-                                return (
-                                    <div className='comment__li' key={comment.id}>
-                                        <div className='comment__container comment_text'>{comment.text}</div>
-
-                                        <div className='comment__info__cont'>
-                                            <Link className='comment__info__box comment__author' to={`/Users/${comment.User.id}`}>{comment.User.username}</Link>
-
-                                            {userId === comment.User.id ? //buttons
-                                                <div className='comment__info__box comment__buttons'>
-                                                    <CommentFormModal songId={songId} userId={userId} edit={true} commentId={comment.id} commentText={comment.text} />
-                                                    <button className='comment__button' onClick={(e) => commentDelete(comment.id)}>Delete Comment</button>
-                                                </div> :
-                                                <div></div>
-                                            }
-
-
-                                            <div className='comment__info__box comment__createdOn'>Created On: {comment.createdAt.slice(0, 10)}</div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-
                     </div>
 
                 </div>
 
 
-                <div className='write__comment__cont'>
-                    {userId && <form className='songPage__comment__form' onSubmit={handleSubmitNew}>
-                        <textarea
-                            className='songPage__comment__textarea'
-                            value={text}
-                            onChange={(e) => { setText(e.target.value) }}
-                            required
-                            placeholder='Write your comment here'
-                        ></textarea>
-                        <button style={{ backgroundColor: '#9F86C0' }} className='songPage__comment__button' type='submit'>Submit</button>
-                    </form>}
+                <div className='audioHolder'>
+                    <audio className='songPage__audio' controls src={song?.link} />
                 </div>
 
             </div>
+
+            <div className={'song__comment__cont'}>
+
+                <div className='comment__holder'>
+
+                    <div className='li__comment__holder'>
+                        {comments && Object.values(comments).map(comment => {
+                            return (
+                                <div className='comment__li' key={comment.id}>
+                                    <div className='comment__container comment_text'>{comment.text}</div>
+
+                                    <div className='comment__info__cont'>
+                                        <Link className='comment__info__box comment__author' to={`/Users/${comment.User.id}`}>{comment.User.username}</Link>
+
+                                        {userId === comment.User.id ? //buttons
+                                            <div className='comment__info__box comment__buttons'>
+                                                <CommentFormModal songId={songId} userId={userId} edit={true} commentId={comment.id} commentText={comment.text} />
+                                                <button className='comment__button' onClick={(e) => commentDelete(comment.id)}>Delete Comment</button>
+                                            </div> :
+                                            <div></div>
+                                        }
+
+
+                                        <div className='comment__info__box comment__createdOn'>Created On: {comment.createdAt.slice(0, 10)}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className='write__comment__cont'>
+                {userId && <form className='songPage__comment__form' onSubmit={handleSubmitNew}>
+                    <textarea
+                        className='songPage__comment__textarea'
+                        value={text}
+                        onChange={(e) => { setText(e.target.value) }}
+                        required
+                        placeholder='Write your comment here'
+                    ></textarea>
+                    <button style={{ backgroundColor: '#9F86C0' }} className='songPage__comment__button' type='submit'>Submit</button>
+                </form>}
+            </div>
+
         </div>
     )
 }
